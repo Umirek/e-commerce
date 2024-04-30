1ffeef860ac7843284261b0447da3a9207b0ad26
Umirek <okteveca@gmail.com>
2024-04-30 12:28:33 +0200

    changes

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const LoginSignup = () => {
   return (
-    <div>LoginSignup</div>
+    <div className='loginsignup'>
+      <div className="loginsignup-container">
+        <h1>Sign Up</h1>
+        <div className="loginsignup-fields">
+          <input type="text" placeholder='Your Name'/>
+          <input type="email" placeholder='Email Adress'/>
+          <input type="password" placeholder='Password'/>
+        </div>
+        <button>Continue</button>
+      </div>
+    </div>
   )
 }
 
