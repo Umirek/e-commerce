4c3d86a04294050deb817beaa08d74959352644a
Umirek <okteveca@gmail.com>
2024-05-06 16:25:26 +0200

    changes

--- frontend/src/Components/Newsletter/Newsletter.jsx
+++ frontend/src/Components/Newsletter/Newsletter.jsx
@@ -3,7 +3,7 @@ import './newsletter.css'
 const Newsletter = () => {
   return (
     <div className='newsletter'>
-        <h1>Get Exclusive Offers Om Your Email</h1>
+        <h1>Get Exclusive Offers On Your Email</h1>
         <p>Subscribe to our newsletter and stay updated</p>
         <div>
             <input type="email" placeholder='Your Email id' />
